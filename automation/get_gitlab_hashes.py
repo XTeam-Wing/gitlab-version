5542c7f01c5dfef6f2a67cf2c36d563e96c835d0
Wing <redteam@aliyun.com>
2024-05-24 18:38:46 +0800

    Update get_gitlab_hashes.py

--- automation/get_gitlab_hashes.py
+++ automation/get_gitlab_hashes.py
@@ -3,24 +3,28 @@
 import json
 import subprocess
 import sys
+import requests
+import time
 
 global builds
 global ignore_list
 builds = ["gitlab-ce", "gitlab-ee"]
 ignore_list = ["rc", "nightly", "latest"]
 
-
 def main(argv):
     if(len(argv) == 0):
         exit("hashes_dict_file file missing")
-
     hashes_dict_file = argv[0]
-    hashes = process_missing_tags(hashes_dict_file)
 
+    fetch_all_tags = False
+    if (len(argv) > 1):
+        fetch_all_tags = argv[1] == "--fetch-all-tags"
+
+    hashes = process_missing_tags(hashes_dict_file, fetch_all_tags)
     write_hashes_dict(hashes, hashes_dict_file)
 
 
-def get_manifest_hash(branch, version):
+def get_manifest_hashes(branch, version):
     try:
         subprocess.check_output("docker rm tmp_gitlab", shell=True)
     except:
@@ -32,24 +36,45 @@ def get_manifest_hash(branch, version):
     # pull tag
     subprocess.check_output("docker create --name='tmp_gitlab' %s" % image, shell=True)
     subprocess.check_output("docker export tmp_gitlab -o tmp_gitlab.tar", shell=True)
-    subprocess.check_output("mkdir -p assets/", shell=True)
-    subprocess.check_output("tar -xf tmp_gitlab.tar opt/gitlab/embedded/service/gitlab-rails/public/assets/ --strip-components=6", shell=True)
 
-    # get version hash
-    with open("./assets/webpack/manifest.json", "r") as file:
-        raw_manifest = file.read()
-    manifest = json.loads(raw_manifest)
+    # get version webpack assets hash
+    try:
+        subprocess.check_output("tar -xf tmp_gitlab.tar opt/gitlab/embedded/service/gitlab-rails/public/assets/webpack/manifest.json --strip-components=8", shell=True)
+        with open("manifest.json", "r") as file:
+            raw_manifest = file.read()
+        manifest = json.loads(raw_manifest)
+        webpack_hash = str(manifest["hash"])
+        subprocess.check_output("rm manifest.json", shell=True)
+    except:
+        webpack_hash = None
+        print("Failed to get webpack hash for %s" % image)
+
+    try:        
+        subprocess.check_output("tar -xf tmp_gitlab.tar opt/gitlab/version-manifest.json --strip-components=2", shell=True)
+        # get version commit hash
+        with open("version-manifest.json", "r") as file:
+            raw_version_manifest = file.read()
+        version_manifest = json.loads(raw_version_manifest)
+        commit_hash = str(version_manifest["software"]["gitlab-rails"]["locked_version"])
+        subprocess.check_output("rm version-manifest.json", shell=True)
+    except:
+        print("Failed to get commit hash for %s" % image)
+        commit_hash = None
+
 
     # cleanup
     try:
+        subprocess.check_output("rm tmp_gitlab.tar", shell=True)
+        subprocess.check_output("docker system prune --all --force", shell=True)
         subprocess.check_output("docker rmi %s -f" % image, shell=True)
         subprocess.check_output("docker rm tmp_gitlab", shell=True)
-        subprocess.check_output("rm tmp_gitlab.tar", shell=True)
-        subprocess.check_output("rm -rf assets/", shell=True)
     except:
         pass
 
-    return str(manifest["hash"])
+    return {
+        "webpack_hash": webpack_hash,
+        "commit_hash": commit_hash
+    }
 
 
 def load_hashes_dict(hashes_dict_file):
@@ -65,10 +90,26 @@ def write_hashes_dict(hashes, path):
         json.dump(hashes, output, indent=4, sort_keys=True)
 
 
-def load_tags(build):
-    with open("%s_tags.json" % build, "r") as file:
-        raw_tags = file.read()
-    tags = json.loads(raw_tags)
+def load_tags(build, fetch_all_tags=False):
+    # get tags from docker hub
+    tags_endpoint = "https://registry.hub.docker.com/v2/repositories/gitlab/%s/tags?page_size=100" % build
+
+    tags = []
+
+    while True:
+        response = requests.get(tags_endpoint)
+        if response.status_code == 200:
+            print("Fetching tags page %s" % tags_endpoint)
+            tags.extend(response.json()["results"])
+
+            if response.json()["next"] and fetch_all_tags:
+                tags_endpoint = response.json()["next"]
+                time.sleep(1)
+            else:
+                break
+        else:
+            print(response.json())
+            exit("Failed to fetch tags for %s" % build)
 
     return tags
 
@@ -89,14 +130,14 @@ def write_processed_tags(processed):
         json.dump(processed, output, indent=4, sort_keys=True)
 
 
-def process_missing_tags(hashes_dict_file):
+def process_missing_tags(hashes_dict_file, fetch_all_tags=False):
     hashes = load_hashes_dict(hashes_dict_file)
     processed = load_processed_tags()
 
     # process missing tags
     for build in builds:
-        tags = load_tags(build)
-        for tag in tags["results"]:
+        tags = load_tags(build, fetch_all_tags)
+        for tag in tags:
             version = str(tag["name"])
             if(
                 not any(ignore in version for ignore in ignore_list)
@@ -104,16 +145,27 @@ def process_missing_tags(hashes_dict_file):
                 not any(processed in version for processed in processed[build])
             ):
                 clean_version = version[:version.index('-')]
-                hash = get_manifest_hash(build, version)
+                hash = get_manifest_hashes(build, version)
 
-                if hashes.get(hash):
-                    hashes[hash]["versions"].append(clean_version)
-                else:
-                    hashes[hash] = {"build": build, "versions": [clean_version]}
+                if hash['webpack_hash'] is not None:
+                    if hashes.get(hash['webpack_hash']):
+                        hashes[hash['webpack_hash']]["versions"].append(clean_version)
+                        hashes[hash['webpack_hash']]["versions"] = list(set(hashes[hash['webpack_hash']]["versions"]))
+                    else:
+                        hashes[hash['webpack_hash']] = {"build": build, "versions": [clean_version]}
+
+                if hash['commit_hash'] is not None:
+                    if hashes.get(hash['commit_hash']):
+                        hashes[hash['commit_hash']]["versions"].append(clean_version)
+                        hashes[hash['commit_hash']]["versions"] = list(set(hashes[hash['commit_hash']]["versions"]))
+                    else:
+                        hashes[hash['commit_hash']] = {"build": build, "versions": [clean_version]}
 
                 processed[build].append(version)
 
-    write_processed_tags(processed)
+            # do partial writes to avoid losing progress
+            write_processed_tags(processed)
+            write_hashes_dict(hashes, hashes_dict_file)
 
     return hashes
 
